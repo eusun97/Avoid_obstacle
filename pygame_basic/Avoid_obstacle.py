e2775e63b5b112856dcff3e461ea35783d27e4c6
Yuseon Lee <usun1128@naver.com>
2022-05-01 03:14:10 +0900

    character commit

--- pygame_basic/Avoid_obstacle.py
+++ pygame_basic/Avoid_obstacle.py
@@ -11,24 +11,30 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # 화면 타이틀 설정
 pygame.display.set_caption("적을 피해 움직여라!") 
 
-# 배경 이미지 불러오기 (그림판에 배경 만들었음)
-# 파일에 background.png 눌러 오른쪽 마우스 --> copy path (주소 복사) 해서 load()안에 넣기
-background = pygame.image.load("C:\\Users\\82105\\Desktop\\PythonWorksapce\\pygame_basic\\background.png") # 역슬래쉬 하나씩 더 추가
+# 배경 이미지 불러오기
+background = pygame.image.load("C:\Pygame_basic\pygame_basic\\background.png")
 
+# 캐릭터(스트라이트) 불러오기 (그림판에 character.png)
+character = pygame.image.load("C:\Pygame_basic\pygame_basic\\character.png")
+character_size = character.get_rect().size # 이미지 크기를 구해옴
+character_width = character_size[0] # 첫번째 위치에 있는 값이 가로 크기가 됨
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2) # 가로 위치 : 화면 가로의 절반 --> 정중앙, 480/2로 왼쪽기준으로 그려지기에 캐릭터 크기 70의 반을 빼야함
+character_y_pos = screen_height - character_height # 세로 위치 : 세로 가장 아래에 해당하는 위치(640위치), 전체높이에서 캐릭터 높이만큼 빼기
+ 
 # 이벤트 루프
 running = True 
 while running: 
     for event in pygame.event.get():  
         if event.type == pygame.QUIT: 
             running = False 
-#   screen.fill((0,0,225) --> 밑에 screen.blit(background, (0,0))를 대신해서 색을 채울 수 있는 문법(알아두기만 하기)
 
-# blit : 배경 그리기 백그라운드가 어디에 나타날지 좌표 설정 (맨왼쪽, 맨위쪽 기준임)
-    screen.blit(background, (0,0)) # (0,0) --> 위치: 0만큼 왼쪽, 0만큼 위아래
+# blit : 좌표 설정 (맨왼쪽, 맨위쪽 기준임)
+    screen.blit(background, (0,0))
 
-# 매번 매 프레임마다 화면을 새로 그려주는 동작을 해야함 = pygame.display.update()
-# while부분을 계속 돌리며 계속 update해야함
-    pygame.display.update() # 게임 화면 다시 그리기! (반드시 필요)
+    screen.blit(character, (character_x_pos, character_y_pos)) # 캐릭터 그리기
+
+    pygame.display.update()
 
 # pygame 종료
 pygame.quit()
