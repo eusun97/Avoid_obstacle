792671d55cebae4d78f8459328831920bc68bcbf
Yuseon Lee <usun1128@naver.com>
2022-05-01 03:32:41 +0900

    fps commit

--- pygame_basic/Avoid_obstacle.py
+++ pygame_basic/Avoid_obstacle.py
@@ -11,6 +11,9 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # 화면 타이틀 설정
 pygame.display.set_caption("적을 피해 움직여라!") 
 
+# FPS : 초당 프레임 수 (프레임 수가 높으면 부드러움, 낮으면 끊기거나 덜 부드러움)
+clock = pygame.time.Clock()
+
 # 배경 이미지 불러오기
 background = pygame.image.load("C:\\Pygame_basic\\pygame_basic\\background.png")
 
@@ -23,50 +26,62 @@ character_x_pos = (screen_width / 2) - (character_width / 2)
 character_y_pos = screen_height - character_height 
 
 # 이동할 좌표 (캐릭터 움직이기)
-to_x = 0 # 가로 방향
-to_y = 0 # 세로 방향
+to_x = 0 
+to_y = 0 
+
+# 캐릭터 이동 속도 1 고정 함수
+character_speed = 1
  
 # 이벤트 루프
 running = True 
 while running: 
+    dt = clock.tick(60) # dt : 델타, 게임화면의 초당 프레임 수를 60으로 설정 : 높게설정하면 부드러움
+
+# 캐릭터가 1초 동안에 00만큼 이동을 해야함
+# 10 fps : 1초 동안에 10번 동작 --> 1번에 몇 만큼 이동해야함? --> 10. 10 * 10 = 100
+# 20 fps : 1초 동안에 20번 동착 --> 5 * 20 
+
+    print("fps : " + str(clock.get_fps())) # 현재 fps 를 출력해줌
+
     for event in pygame.event.get():  
         if event.type == pygame.QUIT: 
             running = False 
 
 # 캐릭터 움직이기
-        if event.type == pygame.KEYDOWN: # 키가 눌러졌는지 확인
-            if event.key == pygame.K_LEFT: # 캐릭터를 왼쪽으로
-                to_x -= 1 # 1 위치만큼 왼쪽으로
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_LEFT: 
+                to_x -= 1 
             elif event.key == pygame.K_RIGHT:
                 to_x += 1
             elif event.key == pygame.K_UP: 
-                to_y -= 1 # 위로 가면 -
+                to_y -= 1 
             elif event.key == pygame.K_DOWN:
                 to_y += 1
 
 # 캐릭터 멈춰있기               
-        if event.type == pygame.KEYUP: # pygame.KEYUP --> 방향키 떼면 멈춤
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: # 왼쪽이나 오른쪽 키보드에서 떼면
-                to_x = 0 # 가로 방향 움직임 멈추기
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN: # 위나 아래쪽 키보드에서 손을 떼면
-                to_y = 0 # 세로 방향 움직임 멈추기
+        if event.type == pygame.KEYUP: 
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT: 
+                to_x = 0 
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                to_y = 0 
 
 
 # 캐릭터 좌표 움직일때마다 바꾸기
-    character_x_pos += to_x  # (0,0)좌표에서 가로 +1, -1만큼 움직일것
-    character_y_pos += to_y  # (0,0)좌표에서 세로 +1, -1만큼 움직일것         
+# 프레임 수를 높게하나 낮게 하나 게임 속도는 변하지 않게 하기 위해 dt를 곱해줌
+    character_x_pos += to_x * dt 
+    character_y_pos += to_y * dt   
 
 # 화면 밖으로 벗어나지 않게 좌표 조정
 # 가로 경계값 처리
-    if character_x_pos < 0: # 화면 왼쪽 이전으로 더 나갔다는 소리
-        character_x_pos = 0 # 맨왼쪽에 멈추기
-    elif character_x_pos > screen_width - character_width: # 오른쪽 끝에 머물게 하기 --> 스크린가로 크기에서 캐릭터 가로 크기 빼야 캐릭터가 화면밖으로 안나감
+    if character_x_pos < 0: 
+        character_x_pos = 0 
+    elif character_x_pos > screen_width - character_width: 
         character_x_pos = screen_width - character_width
 
 # 세로 경계값 처리
-    if character_y_pos < 0: # 화면 위쪽 이전으로 더 나갔다는 소리
-        character_y_pos = 0 # 맨 위쪽에 멈추기
-    elif character_y_pos > screen_height - character_height: # 오른쪽 끝에 머물게 하기 --> 스크린가로 크기에서 캐릭터 가로 크기 빼야 캐릭터가 화면밖으로 안나감
+    if character_y_pos < 0: 
+        character_y_pos = 0 
+    elif character_y_pos > screen_height - character_height: 
         character_y_pos = screen_height - character_height
 
 # blit : 좌표 설정 (맨왼쪽, 맨위쪽 기준임)
